fcb3538a8c06c8402ec7bd29bf9fd7e5fafb132b
1106253569 <1106253569@qq.com>
2023-05-23 03:13:17 +0000

    change

--- pip_install.sh
+++ pip_install.sh
@@ -6,4 +6,9 @@ pip install tensorflow_datasets
 pip install opencv-python
 pip install alfred-py
 pip install pillow
+<<<<<<< HEAD
 sudo apt install libgl1-mesa-glx
+=======
+sudo apt-get update
+sudo apt-get install libgl1
+>>>>>>> 689363b (change)
